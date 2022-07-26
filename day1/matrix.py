324e651ce0b11b2b25d59c60d69cdf69e4295504
ram kumar mahato <ramshree990@gmail.com>
2022-07-26 20:26:03 +0545

    added matrix.py

--- day1/matrix.py
+++ day1/matrix.py
@@ -1,3 +1,26 @@
-l = [[1,2],[3,4],[5,6],[7,8]]
-print(l[::2])
+# 2-D List
+matrix = [[1,2], [3,4], [5,6],[7,8]]
+
+l2 = []
+l3 =[]
+
+for sublist in matrix:
+	for val in sublist:
+		l2.append(val)
+
+odd = [x for x in l2 if x %2!=0]
+even = [x for x in l2 if x %2==0]
+
+l3.append(odd)
+l3.append(even)
+print(matrix)
+print(l2)
+print(odd)
+print(even)
+print(l3)
+
+
+
+
+            
 
