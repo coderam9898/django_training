9492e8dc6ea5ca2a43a9946380fa54c499574488
Ram_kumar <ramk.mahato@patancollege.edu.np>
2022-07-27 12:11:53 +0545

    applied list comprehension

--- day1/posnum.py
+++ day1/posnum.py
@@ -1,3 +1,6 @@
+from pyparsing import Each
+
+
 lst=['1','err','-1',' ',155,2,8,-7,-6]
 newlst = []
 for i in lst:
@@ -7,3 +10,12 @@ for i in lst:
     except ValueError:
         pass
 print(newlst)
+
+
+
+def f1():
+    lst2=['1','err','-1',' ',155,2,8,-7,-6]
+    lst3 = []
+    [Each for each in lst2 if type(each)==int and each>0]
+
+f1()
