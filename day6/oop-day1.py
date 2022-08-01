f5e111c973944c5156d2140b0c3dc84c7c0678a0
Ram_kumar <ramk.mahato@patancollege.edu.np>
2022-08-01 12:31:22 +0545

    added oop file

---
+++ day6/oop-day1.py
@@ -0,0 +1,13 @@
+# Opp concepts
+
+
+class Person:
+    age = 22
+    name = "Ram kumar Mahato"
+     
+    def get_name(self):
+        print(f'my name is {self.name}')
+
+
+p =Person()
+p.get_name()
