b7edf63cfb6fa89d855aba9e75922119a92c77ac
ram kumar mahato <ramshree990@gmail.com>
2022-08-01 21:51:34 +0545

    Q2 completed day 6

--- day6/tasks.py
+++ day6/tasks.py
@@ -14,3 +14,30 @@ class vehicle:
 v = vehicle()
 v.get_data('red','tata','car',4,1500000)
 v.get_data()
+
+
+# Q2. create a class Point where initial x,y values are initialized with 0.
+#  a. create objects of class Point with x and y values passed to it. 
+#    Eg. Point p1 with x = 2 and y with y= 3 should be created.
+#  b. Create a method to Calculate distance between two points.
+#   Formula res = sqrt((x2-x1)^2 +(y2-y1)^2), where x1 and x2 are x values of p1 and p2 points resp. , y1 and y2 are y values of p1 and p2 respectively.
+#  c. Represent the point objects with x,y values in the format (x,y). Eg:(2,3).
+import math
+class Point:
+    def __init__(self):
+        x = 0
+        y = 0
+    
+    def getvalue(self,x,y):
+        print(x,y)
+
+    def distform(self,x,y):
+        print('hello')
+        res = math.sqrt((p1.x-p2.x)^2 +(p1.y-p2.y)^2)
+        print(res)
+
+p1 = Point()
+p2 = Point()
+p1.getvalue(10,12)
+p1.distform(5, 6)
+
